c9b83336e647ebeddf2c09aa49522b4b915d7c7a
James William Graham <wittedhaddock@gmail.com>
2015-08-20 18:57:59 -0400

    Initial Commit

---
+++ AlgebraicCircumscriptions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlgebraicCircumscriptions
+//
+//  Created by James William Graham on 8/20/15.
+//  Copyright (c) 2015 Circumscribing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
